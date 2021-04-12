e8f671323bcfe1c5c889c208348fd50857035662
wuqianye <natsumi3613@gmail.com>
2021-04-12 03:23:19 +0000

    update styling for entry list

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,13 +1,13 @@
 // Copyright 2020 Google LLC
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the 'License');
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an 'AS IS' BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -23,29 +23,35 @@ function loadEntries() {
       entryListElement.appendChild(createEntryElement(entry));
     })
   });
-  console.log("loaded entries!")
+  console.log('loaded entries!')
 }
 
 /** Creates an element that represents a task, including its delete button. */
 function createEntryElement(entry) {
   const entryElement = document.createElement('li');
   entryElement.className = 'entry';
-  entryElement.addEventListener("click", () => {
+
+  const entryTitleElement = document.createElement('span');
+  entryTitleElement.className = 'entryTitle';
+  entryTitleElement.innerText = entry.entryTitle;
+
+  const entryTextElement = document.createElement('span');
+  entryTextElement.className = 'entryText';
+  entryTextElement.innerText = entry.entryText;
+  entryTextElement.addEventListener('click', () => {
     const entryTitleElement = document.getElementById('entryTitle');
     const entryContentElement = document.getElementById('entryContent');
     entryTitleElement.value = entry.entryTitle;
     entryContentElement.innerText = entry.entryText;
-
+    console.log(entry.score);
   });
 
-  const entryTitleElement = document.createElement('p');
-  entryTitleElement.innerText = entry.entryTitle;
-  entryTitleElement.style.fontWeight = "bold";
-
-  const entryTextElement = document.createElement('span');
-  entryTextElement.innerText = entry.entryText;
+  const entryScoreElement = document.createElement('span');
+  entryScoreElement.innerText = 'Score: ' + entry.score;
+  entryScoreElement.style.fontStyle = 'italic';
 
   const deleteButtonElement = document.createElement('button');
+  deleteButtonElement.className = 'btn btn-outline-primary';
   deleteButtonElement.innerText = 'Delete';
   deleteButtonElement.addEventListener('click', () => {
     deleteEntry(entry);
@@ -56,6 +62,7 @@ function createEntryElement(entry) {
 
   entryElement.appendChild(entryTitleElement);
   entryElement.appendChild(entryTextElement);
+  entryElement.appendChild(entryScoreElement);
   entryElement.appendChild(deleteButtonElement);
   return entryElement;
 }
